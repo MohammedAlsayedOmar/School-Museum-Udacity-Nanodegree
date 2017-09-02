72307dd07d0bd146760d04fa75699dba778331c3
mohammedalsayedomar <dev.mohammedsayedomar@gmail.com>
2017-09-02 03:42:31 +0200

    chore: Upload everything

--- README.md
+++ README.md
@@ -1,2 +1,32 @@
-# School-Museum-Udacity-Nanodegree
-School Musem
+# School Museum
+School Museum was created as a part of Udacity’s VR nanodegree program. We were required to research about creating VR experience for things other than games and to apply the researched concept. My chosen concept was learning new things in a school environment. Here is a brief review of the experience.
+![Gameplay Screenshot](./Screenshots/7.png)
+Time Taken: 10 days
+
+
+### Features
+* More than 1 intractable item
+* Semi realistic environment
+* Calm background music (from https://www.bensound.com)
+* Some basic knowledge and information
+* Visual feedback
+* A bit optimized for mobile phones
+
+### Software/ Plugins Used
+* Unity Engine 5.6.1
+* Google VR SDK 1.6
+* Post Processing Stack
+* 3Ds Max
+* Microsoft Paint
+
+
+### Download Link
+not yet available (check back soon)
+
+### More Information
+You can read about the process of creating this project here: https://medium.com/@MohammedAlsayed/*NOTYET*
+
+More information about the course at: https://classroom.udacity.com/nanodegrees/nd017
+
+## License
+This project is licensed under the MIT License - see the [LICENSE.md](LICENSE.md) file for details
---
+++ School Museum/Assets/Refrence/Refrence.cs
@@ -0,0 +1,48 @@
+﻿public class Refrence{
+
+ //   BGM
+ //    Music: https://www.bensound.com
+
+	//Skeleton Model
+
+ //   https://free3d.com/3d-model/mrbones-skeleton-lowpoly-22408.html
+	
+ //   Ground Texture
+ //   https://www.dreamstime.com/royalty-free-stock-photos-seamless-parquet-texture-can-be-used-d-rendering-image36780398
+
+ //   Board Font
+ //   http://www.1001fonts.com/chawp-font.html#license
+
+ //   WoodFine
+ //   http://christianweeks.github.io/Portfolio/cpsc809/wacomPen/WoodFine0023_L.jpg
+
+ //   SolarSystem Image
+ //   https://i.ytimg.com/vi/zzbCEF37MfU/maxresdefault.jpg
+
+ //   FoodChain Image
+ //   https://i.ytimg.com/vi/FFloV2J-eKI/maxresdefault.jpg
+
+ //   Another Wood Image
+ //   https://s-media-cache-ak0.pinimg.com/originals/50/0d/2b/500d2b76558a2c7f795f27f199e8969d.jpg
+
+ //   Graham Bell
+ //   https://en.wikipedia.org/wiki/Alexander_Graham_Bell
+
+ //   SmokePuff
+ //   https://www.pinterest.com/pin/385761524305610178/
+
+ //   Text to voice
+ //   http://soundoftext.com/ (google translate)
+ //   http://www.fromtexttospeech.com/ (change text to voice)
+
+ //   Text from
+ //   https://en.wikipedia.org/wiki/Sansevieria_trifasciata
+ //   https://en.wikipedia.org/wiki/Food_web
+ //   https://en.wikipedia.org/wiki/Food_chain
+ //   https://en.wikipedia.org/wiki/Solar_System
+ //   https://en.wikipedia.org/wiki/Helianthus
+ //   https://en.wikipedia.org/wiki/Human_skeleton
+ //   https://en.wikipedia.org/wiki/Ahmed_Zewail
+ //   https://en.wikipedia.org/wiki/Stephen_Hawking
+ //   https://www.quora.com/What-happens-when-you-mix-hydrochloric-acid-with-ammonia
+}
